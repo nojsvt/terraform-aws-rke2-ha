572dcf878d5267bdc9928a376d83bc6d5d64d62d
pkthom <149856238+pkthom@users.noreply.github.com>
2025-06-18 08:09:56 +0700

    Update ec2_server1.tf

--- ec2_server1.tf
+++ ec2_server1.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "rke2_server1" {
   subnet_id     = var.subnet_id
   key_name      = var.key_name
   vpc_security_group_ids = [var.security_group_id]
-  iam_instance_profile = aws_iam_instance_profile.acl_server_instance_profile.name
+  iam_instance_profile = aws_iam_instance_profile.acp_server_instance_profile.name
 
   tags = {
     Name = "server1"
