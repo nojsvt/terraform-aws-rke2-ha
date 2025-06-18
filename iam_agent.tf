d5777ea57f843aa10ce633e30cdf38a31911509c
pkthom <149856238+pkthom@users.noreply.github.com>
2025-06-18 08:08:16 +0700

    Update iam_agent.tf

--- iam_agent.tf
+++ iam_agent.tf
@@ -1,7 +1,7 @@
-# IAM Policy: acl-agent-policy
-resource "aws_iam_policy" "acl_agent_policy" {
-  name        = "acl-agent-policy"
-  description = "IAM policy for acl-agent-role"
+# IAM Policy: acp-agent-policy
+resource "aws_iam_policy" "acp_agent_policy" {
+  name        = "acp-agent-policy"
+  description = "IAM policy for acp-agent-role"
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -25,9 +25,9 @@ resource "aws_iam_policy" "acl_agent_policy" {
   })
 }
 
-# IAM Role: acl-agent-role
-resource "aws_iam_role" "acl_agent_role" {
-  name               = "acl-agent-role"
+# IAM Role: acp-agent-role
+resource "aws_iam_role" "acp_agent_role" {
+  name               = "acp-agent-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -42,18 +42,18 @@ resource "aws_iam_role" "acl_agent_role" {
   })
 
   tags = {
-    Name = "acl-agent-role"
+    Name = "acp-agent-role"
   }
 }
 
 # Attach the policy to the role
-resource "aws_iam_role_policy_attachment" "acl_agent_policy_attachment" {
-  role       = aws_iam_role.acl_agent_role.name
-  policy_arn = aws_iam_policy.acl_agent_policy.arn
+resource "aws_iam_role_policy_attachment" "acp_agent_policy_attachment" {
+  role       = aws_iam_role.acp_agent_role.name
+  policy_arn = aws_iam_policy.acp_agent_policy.arn
 }
 
-# IAM Instance Profile: acl-agent-role-instance-profile
-resource "aws_iam_instance_profile" "acl_agent_instance_profile" {
-  name = aws_iam_role.acl_agent_role.name
-  role = aws_iam_role.acl_agent_role.name
+# IAM Instance Profile: acp-agent-role-instance-profile
+resource "aws_iam_instance_profile" "acp_agent_instance_profile" {
+  name = aws_iam_role.acp_agent_role.name
+  role = aws_iam_role.acp_agent_role.name
 }
